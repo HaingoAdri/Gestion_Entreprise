47925c3832fd5c855c1ca36b6600c51306a212f3
Sanjaratianaa <119104188+Sanjaratianaa@users.noreply.github.com>
2023-12-04 03:13:28 -0700

    Create Bon_controller.php

---
+++ app/Http/Controllers/Bon_controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+// use App\Models\Employer;
+use Carbon\Carbon;
+
+use Illuminate\Support\Facades\DB; // Importez la classe DB
+use Illuminate\Support\Facades\Session;
+
+class Bon_controller extends Controller
+{
+
+    // INDEX
+    public function index() {
+    }
+
+}
